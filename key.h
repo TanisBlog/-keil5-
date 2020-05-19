7406f90590b6553a964012d5cc04feb9e88a00c2
TanisBlog <60872111+TanisBlog@users.noreply.github.com>
2020-05-19 11:21:57 +0800

    Create key.h

---
+++ key.h
@@ -0,0 +1,12 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+#include <reg52.h>
+#define uchar unsigned char
+#define uint unsigned int
+
+
+void delay_ms(uint i);
+void key_scan0();
+void key_scan1();
+void key_judge();
+#endif
